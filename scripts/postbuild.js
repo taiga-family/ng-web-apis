9a4985eeba1b8b028b62520509f215586126c69a
MarsiBarsi <darragon-nn@yandex.ru>
2020-01-27 19:21:10 +0300

    chore(scripts): add readme logo fix

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -12,7 +12,14 @@ function copyExtraFiles() {
     if (!fs.existsSync(README_PATH) || !fs.existsSync(LOGO_PATH)) {
         throw new Error('Requested files do not exit');
     } else {
-        fs.copyFileSync(README_PATH, PATH_TO_README);
+        copyReadmeIntoDistFolder(README_PATH, PATH_TO_README);
         fs.copyFileSync(LOGO_PATH, PATH_TO_LOGO);
     }
+}
+
+function copyReadmeIntoDistFolder(srcPath, toPath) {
+    const fileBody = fs.readFileSync(srcPath).toString();
+    const withoutLogos = fileBody.replace('![logo](logo.svg) ', '');
+
+    fs.writeFileSync(toPath, withoutLogos);
 }
