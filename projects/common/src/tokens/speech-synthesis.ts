516438d984e6cc62858dedb8ee93a5338e3e5a42
waterplea <alexander@inkin.ru>
2020-10-26 18:48:57 +0300

    fix(SPEECH_SYNTHESIS): fix type

--- projects/common/src/tokens/speech-synthesis.ts
+++ projects/common/src/tokens/speech-synthesis.ts
@@ -1,7 +1,7 @@
 import {inject, InjectionToken} from '@angular/core';
 import {WINDOW} from './window';
 
-export const SPEECH_SYNTHESIS = new InjectionToken<SpeechSynthesis | null>(
+export const SPEECH_SYNTHESIS = new InjectionToken<SpeechSynthesis>(
     'An abstraction over window.speechSynthesis object',
     {
         factory: () => inject(WINDOW).speechSynthesis,
