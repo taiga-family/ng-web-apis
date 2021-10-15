1c43f383cfef81c8ccf7ce98921496ac7091fcac
Alex Inkin <alexander@inkin.ru>
2021-10-15 16:20:13 +0300

    fix(types): compatibility issues with modern TypeScript (#26)

--- projects/common/src/tokens/network-information.ts
+++ projects/common/src/tokens/network-information.ts
@@ -1,11 +1,10 @@
-/// <reference types="network-information-types" />
-
 import {inject, InjectionToken} from '@angular/core';
 import {NAVIGATOR} from './navigator';
 
-export const NETWORK_INFORMATION = new InjectionToken<NetworkInformation | null>(
-    'An abstraction over window.navigator.connection object',
-    {
-        factory: () => inject(NAVIGATOR).connection || null,
-    },
-);
+export const NETWORK_INFORMATION = new InjectionToken<
+    // @ts-ignore
+    typeof navigator['connection'] | null
+>('An abstraction over window.navigator.connection object', {
+    // @ts-ignore
+    factory: () => inject(NAVIGATOR).connection || null,
+});
