2c05446e85dc5d4f861a56f86687deb9a0e341c9
Debmallya Bhattacharya <batbrain9392@gmail.com>
2022-01-27 09:12:26 +0000

    feat(MEDIA_DEVICES): added new token (#33)
    Co-authored-by: Debmallya Bhattacharya <debmallya.bhattacharya@blueface.com>

---
+++ projects/common/src/tokens/media-devices.ts
@@ -0,0 +1,9 @@
+import {inject, InjectionToken} from '@angular/core';
+import {NAVIGATOR} from './navigator';
+
+export const MEDIA_DEVICES = new InjectionToken<MediaDevices>(
+    'An abstraction over window.navigator.mediaDevices object',
+    {
+        factory: () => inject(NAVIGATOR).mediaDevices,
+    },
+);
