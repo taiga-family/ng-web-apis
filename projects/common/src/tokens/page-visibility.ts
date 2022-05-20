63400c6e61088af3782ab5a6189427e3981e4ed5
Jack Frate <jack.frate@showpad.com>
2022-05-20 12:30:28 -0500

    fix(PAGE_VISIBILITY): share replay of page visibility

--- projects/common/src/tokens/page-visibility.ts
+++ projects/common/src/tokens/page-visibility.ts
@@ -1,7 +1,7 @@
 import {DOCUMENT} from '@angular/common';
 import {inject, InjectionToken} from '@angular/core';
 import {fromEvent, Observable} from 'rxjs';
-import {distinctUntilChanged, map, share, startWith} from 'rxjs/operators';
+import {distinctUntilChanged, map, shareReplay, startWith} from 'rxjs/operators';
 
 export const PAGE_VISIBILITY = new InjectionToken<Observable<boolean>>(
     'Shared Observable based on `document visibility changed`',
@@ -13,7 +13,7 @@ export const PAGE_VISIBILITY = new InjectionToken<Observable<boolean>>(
                 startWith(0),
                 map(() => documentRef.visibilityState !== 'hidden'),
                 distinctUntilChanged(),
-                share(),
+                shareReplay({refCount: false, bufferSize: 1}),
             );
         },
     },
