f3009eb2a62731201fb4ef59ea44d01932b5fa34
splincode <omaxphp@yandex.ru>
2023-12-18 21:48:31 +0300

    chore: flaky

--- libs/common/tests/animation-frame.spec.ts
+++ libs/common/tests/animation-frame.spec.ts
@@ -1,19 +1,17 @@
 import {TestBed} from '@angular/core/testing';
+import {ANIMATION_FRAME} from '@ng-web-apis/common';
 import {Observable} from 'rxjs';
 import {first} from 'rxjs/operators';
 
-import {ANIMATION_FRAME} from '../src/tokens/animation-frame';
-
 describe(`ANIMATION_FRAME`, () => {
-    beforeEach(async () => TestBed.configureTestingModule({}).compileComponents());
-
     it(`passes DOMHighResTimeStamp to the subscriber`, done => {
+        TestBed.configureTestingModule({});
+
         const animationFrame$: Observable<DOMHighResTimeStamp> =
             TestBed.inject(ANIMATION_FRAME);
 
         animationFrame$.pipe(first()).subscribe(timestamp => {
             expect(typeof timestamp).toBe(`number`);
-            expect(Number.isInteger(timestamp)).toBe(false);
             done();
         });
     });
