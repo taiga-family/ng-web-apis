30433f859c597cc72fb055ea147a95203e6395a7
Denes Papp <pppdns@gmail.com>
2024-07-29 11:06:13 +0200

    Ran Prettier
    npm run prettier -- --write

--- libs/mutation-observer/src/classes/safe-observer.ts
+++ libs/mutation-observer/src/classes/safe-observer.ts
@@ -2,9 +2,9 @@ export const SafeObserver =
     typeof MutationObserver !== `undefined`
         ? MutationObserver
         : class implements MutationObserver {
-            observe(): void {}
-            disconnect(): void {}
-            takeRecords(): MutationRecord[] {
-                return [];
-            }
-        };
+              observe(): void {}
+              disconnect(): void {}
+              takeRecords(): MutationRecord[] {
+                  return [];
+              }
+          };
