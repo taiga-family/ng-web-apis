1f9c61f781faca8ad69fb9de357a473ca4b5ee0b
null <taiga-family-bot@yandex.ru>
2024-08-27 08:20:16 +0000

    chore(deps): update dependency @taiga-ui/cdk to v4

--- libs/common/tests/page-visibility.spec.ts
+++ libs/common/tests/page-visibility.spec.ts
@@ -1,5 +1,5 @@
+import { WA_PAGE_VISIBILITY } from "@ng-web-apis/common";
 import {TestBed} from '@angular/core/testing';
-import {PAGE_VISIBILITY} from '@ng-web-apis/common';
 import type {Observable} from 'rxjs';
 import {first} from 'rxjs';
 
@@ -7,7 +7,7 @@ describe('PAGE_VISIBILITY', () => {
     it('watching for page visibility state', (done) => {
         TestBed.configureTestingModule({});
 
-        const pageVisibility$: Observable<boolean> = TestBed.inject(PAGE_VISIBILITY);
+        const pageVisibility$: Observable<boolean> = TestBed.inject(WA_PAGE_VISIBILITY);
 
         pageVisibility$.pipe(first()).subscribe((state) => {
             expect(typeof state).toBe('boolean');
