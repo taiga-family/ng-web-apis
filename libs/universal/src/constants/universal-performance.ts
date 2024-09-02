db9d46e80b5ae26c7c3c3d98fb6b18796221277b
null <taiga-family-bot@yandex.ru>
2024-09-02 17:30:51 +0000

    chore(deps): update taiga-ui cdk to v4

--- libs/universal/src/constants/universal-performance.ts
+++ libs/universal/src/constants/universal-performance.ts
@@ -1,12 +1,12 @@
 import type {FactoryProvider} from '@angular/core';
-import {PERFORMANCE} from '@ng-web-apis/common';
+import {WA_PERFORMANCE} from '@ng-web-apis/common';
 
 export function performanceFactory(): Performance {
     return require('node:perf_hooks').performance;
 }
 
 export const UNIVERSAL_PERFORMANCE: FactoryProvider = {
-    provide: PERFORMANCE,
+    provide: WA_PERFORMANCE,
     deps: [],
     useFactory: performanceFactory,
 };
