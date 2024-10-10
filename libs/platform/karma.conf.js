70543f1548e31c0478a093c92214b2e0e65c0b9d
Maksim Ivanov <omaxphp@yandex.ru>
2024-10-10 10:19:25 +0300

    feat(platform): add `WA_IS_TOUCH`, `WA_IS_WEBKIT`, `WA_IS_ANDROID`, `WA_IS_IOS`, `WA_IS_MOBILE` (#944)

---
+++ libs/platform/karma.conf.js
@@ -0,0 +1,47 @@
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
+
+module.exports = function (config) {
+    config.set({
+        basePath: '',
+        frameworks: ['jasmine', '@angular-devkit/build-angular'],
+        plugins: [
+            require('karma-jasmine'),
+            require('karma-chrome-launcher'),
+            require('karma-jasmine-html-reporter'),
+            require('@angular-devkit/build-angular/plugins/karma'),
+            require('karma-coverage'),
+        ],
+        client: {
+            clearContext: true, // leave Jasmine Spec Runner output visible in browser
+        },
+        jasmineHtmlReporter: {
+            suppressAll: true, // removes the duplicated traces
+        },
+        preprocessors: {
+            'src/**/*.js': ['coverage'],
+        },
+        coverageReporter: {
+            type: 'html',
+            dir: require('node:path').join(__dirname, '../../coverage/payment-request'),
+        },
+        reporters: ['progress', 'kjhtml'],
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: true,
+        browsers: ['ChromeHeadless'],
+        singleRun: true,
+        customLaunchers: {
+            ChromeHeadless: {
+                base: 'Chrome',
+                flags: [
+                    '--no-sandbox',
+                    '--headless',
+                    '--disable-gpu',
+                    '--remote-debugging-port=9222',
+                ],
+            },
+        },
+    });
+};
--- libs/platform/src/is-ios.ts
+++ libs/platform/src/is-ios.ts
@@ -1,3 +1,6 @@
+import {inject, InjectionToken} from '@angular/core';
+import {WA_NAVIGATOR} from '@ng-web-apis/common';
+
 import {isApple} from './is-apple';
 
 const IOS_REG_EXP = /ipad|iphone|ipod/;
@@ -8,3 +11,7 @@ export function isIos(navigator: Navigator): boolean {
         (isApple(navigator) && navigator.maxTouchPoints > 1)
     );
 }
+
+export const WA_IS_IOS = new InjectionToken<boolean>('', {
+    factory: () => isIos(inject(WA_NAVIGATOR)),
+});
