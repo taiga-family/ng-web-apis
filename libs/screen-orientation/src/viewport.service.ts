9dd32109d37c8a37e325e3f36ed964acb2da35ba
waterplea <alexander@inkin.ru>
2024-10-14 16:30:57 +0400

    fix(screen-orientation): `Viewport` fix initial value

--- libs/screen-orientation/src/viewport.service.ts
+++ libs/screen-orientation/src/viewport.service.ts
@@ -1,6 +1,15 @@
 import {inject, Injectable} from '@angular/core';
 import {WINDOW} from '@ng-web-apis/common';
-import {EMPTY, filter, fromEvent, map, merge, Observable, shareReplay} from 'rxjs';
+import {
+    EMPTY,
+    filter,
+    fromEvent,
+    map,
+    merge,
+    Observable,
+    shareReplay,
+    startWith,
+} from 'rxjs';
 
 @Injectable({
     providedIn: 'root',
@@ -14,6 +23,7 @@ export class ViewportService extends Observable<VisualViewport> {
               fromEvent(this.visualViewport, 'scroll'),
               fromEvent(this.visualViewport, 'scrollend'),
           ).pipe(
+              startWith(null),
               map(() => this.visualViewport),
               filter(Boolean),
               shareReplay({bufferSize: 1, refCount: true}),
