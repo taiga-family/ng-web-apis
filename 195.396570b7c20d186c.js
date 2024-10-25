42e8dd7cddf306b1114c261a550fdde05fa099ad
vladimirpotekhin <46284632+vladimirpotekhin@users.noreply.github.com>
2024-10-25 14:20:07 +0000

    Deploying to gh-pages from @ taiga-family/ng-web-apis@b8ed5868b2ef2b8311515acfd6ad32a5ae8c00d4 🚀

--- 195.396570b7c20d186c.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[195],{4195:n=>{n.exports="# ![ng-web-apis logo](https://raw.githubusercontent.com/taiga-family/ng-web-apis/main/libs/platform/logo.svg) Platform\n\n## Install\n\n```bash\nnpm i @ng-web-apis/platform\n```\n\n- `isApple`\n- `isIos`\n"}}]);
