52d7d950f0646f6f52f0c6ad3aa16dac934f5ff9
splincode <omaxphp@yandex.ru>
2025-03-28 14:02:19 +0300

    feat(mutation-observer): add `provideMutationObserverInit`

--- libs/mutation-observer/src/tokens/mutation-observer-init.ts
+++ libs/mutation-observer/src/tokens/mutation-observer-init.ts
@@ -1,4 +1,4 @@
-import {InjectionToken} from '@angular/core';
+import {InjectionToken, type Provider} from '@angular/core';
 
 export const WA_MUTATION_OBSERVER_INIT = new InjectionToken<MutationObserverInit>(
     '[WA_MUTATION_OBSERVER_INIT]',
@@ -8,3 +8,10 @@ export const WA_MUTATION_OBSERVER_INIT = new InjectionToken<MutationObserverInit
  * @deprecated: drop in v5.0, use {@link WA_MUTATION_OBSERVER_INIT}
  */
 export const MUTATION_OBSERVER_INIT = WA_MUTATION_OBSERVER_INIT;
+
+export function provideMutationObserverInit(useValue: MutationObserverInit): Provider {
+    return {
+        provide: WA_MUTATION_OBSERVER_INIT,
+        useValue,
+    }
+}
