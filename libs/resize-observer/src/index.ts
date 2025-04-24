4c2b25a77ad2e91c202aeca543ee3284238de222
mdlufy <german.panov.2018@mail.ru>
2025-04-24 10:37:22 +0300

    feat(resize-observer): `RESIZE_WATCHED_ELEMENT` add token

--- libs/resize-observer/src/index.ts
+++ libs/resize-observer/src/index.ts
@@ -1,4 +1,5 @@
 export * from './directives/resize-observer.directive';
 export * from './services/resize-observer.service';
 export * from './tokens/resize-option-box';
+export * from './tokens/resize-watched-element';
 export * from './tokens/support';
--- libs/resize-observer/src/services/resize-observer.service.ts
+++ libs/resize-observer/src/services/resize-observer.service.ts
@@ -3,11 +3,14 @@ import {Observable} from 'rxjs';
 
 import {SafeObserver} from '../classes/safe-observer';
 import {RESIZE_OPTION_BOX} from '../tokens/resize-option-box';
+import {RESIZE_WATCHED_ELEMENT} from '../tokens/resize-watched-element';
 
 @Injectable()
 export class ResizeObserverService extends Observable<readonly ResizeObserverEntry[]> {
     constructor() {
-        const nativeElement: HTMLElement = inject(ElementRef).nativeElement;
+        const nativeElement =
+            inject(RESIZE_WATCHED_ELEMENT, {optional: true}) ??
+            inject(ElementRef).nativeElement;
         const box = inject(RESIZE_OPTION_BOX);
 
         super((subscriber) => {
