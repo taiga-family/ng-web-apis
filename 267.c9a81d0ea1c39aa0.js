35be0692114c863a7e2395a80a4ff1a7e53ab600
waterplea <alexander@inkin.ru>
2025-12-19 14:08:42 +0000

    Deploying to gh-pages from @ taiga-family/ng-web-apis@32a2a501ed7af6c72c997dd97e507850ae7ce09c 🚀

--- 267.c9a81d0ea1c39aa0.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdemo=self.webpackChunkdemo||[]).push([[267],{267:n=>{n.exports='<button\n    tuiButton\n    type="button"\n    [disabled]="(denied$ | async)!"\n    (click)="sendNotification()"\n>\n    Send notification\n</button>\n'}}]);
