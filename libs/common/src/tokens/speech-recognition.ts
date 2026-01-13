c1fa44edc2ca16bf503636fc109b0476de2cfef3
Максим Иванов <omaxphp@yandex.ru>
2026-01-13 10:21:18 +0300

    fix(common): prevents TS errors when a consumer has `skipLibCheck: false` (#1513)

--- libs/common/src/tokens/speech-recognition.ts
+++ libs/common/src/tokens/speech-recognition.ts
@@ -2,6 +2,20 @@ import {inject, InjectionToken, type Type} from '@angular/core';
 
 import {WA_WINDOW} from './window';
 
+declare global {
+    /**
+     * Workaround:
+     * This empty interface prevents TS errors when a consumer has
+     * `skipLibCheck: false` and did not include `dom-speech-recognition`
+     * in `tsconfig.types`.
+     *
+     * The real `SpeechRecognition` interface is provided by
+     * `@types/dom-speech-recognition` when it is present.
+     */
+    // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+    interface SpeechRecognition {}
+}
+
 export const WA_SPEECH_RECOGNITION = new InjectionToken<Type<SpeechRecognition> | null>(
     '[WA_SPEECH_RECOGNITION]: [WA_SPEECH_RECOGNITION]',
     {
