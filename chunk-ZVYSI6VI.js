62b98eaabf337ae8d79d1a061922a93d1f4dd84d
splincode <omaxphp@yandex.ru>
2026-05-18 07:28:16 +0000

    Deploying to gh-pages from @ taiga-family/ng-web-apis@e4773fc5a2854cd2421bbda7d69bc50a66670adc 🚀

---
+++ chunk-ZVYSI6VI.js
@@ -0,0 +1,29 @@
+import"./chunk-GAL4ENT6.js";var n=`# ![ng-web-apis logo](https://raw.githubusercontent.com/taiga-family/ng-web-apis/main/libs/platform/logo.svg) Platform
+
+## Install
+
+\`\`\`bash
+npm i @ng-web-apis/platform
+\`\`\`
+
+## Tokens
+
+- \`WA_IS_MOBILE\`
+- \`WA_IS_IOS\`
+- \`WA_IS_ANDROID\`
+- \`WA_IS_WEBKIT\`
+
+## Functions
+
+- \`waInjectElement<T = HTMLElement>()\` \u2014 injects the host native element via \`ElementRef\`. Must be called in an
+  injection context (constructor, field initializer, or \`inject\`-compatible factory).
+
+\`\`\`ts
+import {waInjectElement} from '@ng-web-apis/platform';
+
+@Directive({selector: '[myDirective]'})
+export class MyDirective {
+  private readonly element = waInjectElement<HTMLInputElement>();
+}
+\`\`\`
+`;export{n as default};
